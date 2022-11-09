36643868ff0931ab5ab21b78103a8d807fb5d8cd
Michał Moskała <moskala.michal@gmail.com>
2022-11-09 21:45:28 +0100

    Create app from template

---
+++ SwiftUITabbedApp/SwiftUITabbedAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUITabbedAppApp.swift
+//  SwiftUITabbedApp
+//
+//  Created by Michal Moskala on 09/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUITabbedAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
